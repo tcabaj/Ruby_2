51ab5f9d715add96b1746066a6aa910f9a634c69
tcabaj <tomcab95@gmail.com>
2017-01-07 16:19:26 +0100

    Update

--- lib/program.rb
+++ lib/program.rb
@@ -1,57 +1,56 @@
-# Run class
 require_relative 'menu'
 require_relative 'muzyka'
 require_relative 'manager'
-
+# Run class
 class Run
   @manager = Manager.new
   @menu = Menu.new
-
-  while true
+  i = 1
+  while i == 1
     @menu.clear
     puts @menu.to_s
     printf "\n Wybierz opcje: "
-    puts case gets.chomp
-      when '1'
-        puts
-        puts 'Podaj tytul utworu: '
-        title = gets.chomp
-        puts 'Podaj wykonawce: '
-        author = gets.chomp
-        puts 'Podaj rok produkcji: '
-        yop = gets.chomp
-        @manager.add_muzyka(title, author, yop)
-        print "Utwor zostal dodany"
-      when '2'
-        puts
-        puts 'Podaj ID piosenki, ktory chcesz usunac '
-        id = gets.chomp
-        @manager.delete_muzyka(id)
-        print 'Utwor zostal usuniety'
-      when '3'
-        puts
-        puts 'Podaj ID piosenki'
-        id = gets.chomp
-        id = id.to_i
-        puts 'Podaj tytul utworu: '
-        title = gets.chomp
-        puts 'Podaj wykonawce: '
-        author = gets.chomp
-        puts 'Podaj rok produkcji:  '
-        yop = gets.chomp
-        @manager.update_muzyka(id, title, author, yop)
-        puts 'Utwor zostal zmieniony'
-      when '4'
-        puts
-        print "---- Lista Utworow ---- \n"
-        @manager.get_muzyka
-        printf "\n"
-        gets.chomp
-      when '0'
-        break
-        else
-        puts 'Podales zly znak'
-        gets
+    case gets.chomp
+    when '1'
+      puts
+      puts 'Podaj tytul utworu: '
+      title = gets.chomp
+      puts 'Podaj wykonawce: '
+      author = gets.chomp
+      puts 'Podaj rok produkcji: '
+      yop = gets.chomp
+      @manager.add_muzyka(title, author, yop)
+      print 'Utwor zostal dodany'
+    when '2'
+      puts
+      puts 'Podaj ID piosenki, ktory chcesz usunac '
+      id = gets.chomp
+      @manager.delete_muzyka(id)
+      print 'Utwor zostal usuniety'
+    when '3'
+      puts
+      puts 'Podaj ID piosenki'
+      id = gets.chomp
+      id = id.to_i
+      puts 'Podaj tytul utworu: '
+      title = gets.chomp
+      puts 'Podaj wykonawce: '
+      author = gets.chomp
+      puts 'Podaj rok produkcji:  '
+      yop = gets.chomp
+      @manager.update_muzyka(id, title, author, yop)
+      puts 'Utwor zostal zmieniony'
+    when '4'
+      puts
+      print "---- Lista Utworow ---- \n"
+      @manager.get_muzyka
+      printf "\n"
+      gets.chomp
+    when '0'
+      break
+    else
+      puts 'Podales zly znak'
+      gets
     end
   end
 
