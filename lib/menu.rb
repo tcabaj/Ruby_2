86fcf98824e17b2f06bf67ead580335f2620aeed
tcabaj <tomcab95@gmail.com>
2017-01-08 17:56:47 +0100

    Update

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,7 +7,7 @@ class Menu
     @menu << "----------------------------------\n"
     @menu << "1. Dodaj utwór muzyczny \n"
     @menu << "2. Usunac utwór muzyczny \n"
-    @menu << "3. Edydtuj podany utwór muzyczny \n"
+    @menu << "3. Edytuj podany utwór muzyczny \n"
     @menu << "4. Wyświetl wszystkie utwóry \n"
     @menu << "0. Wyjscie\n"
     @menu << "----------------------------------\n"
