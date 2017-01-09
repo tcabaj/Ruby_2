6182c796b2e11d72739c1a0b5290d37b33a75a19
tcabaj <tomcab95@gmail.com>
2017-01-09 12:04:51 +0100

    Update menager_spec.rb

--- spec/menager_spec.rb
+++ spec/menager_spec.rb
@@ -4,19 +4,19 @@ require 'manager'
 RSpec.describe 'manager.rb' do
   obj = Manager.new
 
-  it 'should add new film' do
+  it 'should add new music' do
     expect { obj.add_muzyka('Some', 'Anonim', '2017') } .not_to raise_error
   end
 
-  it 'should output all films' do
+  it 'should output all music' do
     expect { obj.list_muzyka } .not_to raise_error
   end
 
-  it 'should edit film' do
+  it 'should edit music' do
     expect { obj.update_muzyka(1, 'Some', 'Gall', '2017') } .not_to raise_error
   end
 
-  it 'should delete film' do
+  it 'should delete music' do
     expect { obj.delete_muzyka(1) } .not_to raise_error
   end
 
